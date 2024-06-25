59c26ce525863752a0543833fb03e1c9515a715b
piatoss <crewe1746@naver.com>
2024-06-25 13:22:26 +0900

    Remove Polygon Mumbai network, it's deprecated

--- piatoss/src/providers/WalletProvider.tsx
+++ piatoss/src/providers/WalletProvider.tsx
@@ -3,19 +3,21 @@
 import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { polygon, polygonMumbai } from "wagmi/chains";
+import { polygon } from "wagmi/chains";
 
 const config = getDefaultConfig({
   appName: "Piatoss",
   projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "",
-  chains: [polygon, polygonMumbai],
+  chains: [polygon],
   ssr: true,
 });
 
 export const WalletProvider = ({ children }: { children: React.ReactNode }) => {
+  const client = new QueryClient();
+
   return (
     <WagmiProvider config={config}>
-      <QueryClientProvider client={new QueryClient()}>
+      <QueryClientProvider client={client}>
         <RainbowKitProvider coolMode initialChain={polygon}>
           {children}
         </RainbowKitProvider>
